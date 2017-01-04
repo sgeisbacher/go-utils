935c3d6fa191bc4e9222e1a1d836c39dbb3245a3
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2017-01-04 21:20:35 +0100

    added toID() and basic setup

---
+++ data_utils.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+var REGEX_SPECIAL_CHARS = regexp.MustCompile("[^a-zA-Z0-9-]")
+
+func ToID(str string) string {
+	replacer := strings.NewReplacer(" ", "-", "ä", "ae", "Ä", "Ae", "ö", "oe", "Ö", "Oe", "ü", "ue", "Ü", "Ue", "ß", "ss")
+	id := replacer.Replace(str)
+	id = REGEX_SPECIAL_CHARS.ReplaceAllString(id, "")
+	id = strings.Trim(id, "-")
+	return strings.ToLower(id)
+}
---
+++ data_utils_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestToIDReplacesSpacesWithDash(t *testing.T) {
+	RegisterTestingT(t)
+
+	var tableTestData = []struct {
+		str      string
+		expected string
+	}{
+		{"Name of a Product", "name-of-a-product"},
+		{" Product's name   ", "products-name"},
+		{" Äpfel sind öfters grün ! ", "aepfel-sind-oefters-gruen"},
+	}
+	for _, testData := range tableTestData {
+		Expect(ToID(testData.str)).To(Equal(testData.expected))
+	}
+}
